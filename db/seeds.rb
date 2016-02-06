538ccac4d03cd37b71a4bcbaeb9ac45f8e4c75a6
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-07 03:37:25 +0900

    seed ファイルに BlogCategory のツリー構造生成処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+BlogCategory.delete_all
+BlogCategory.connection.execute(%|SELECT setval ('blog_categories_id_seq', 1, false)|)
+
+1.upto(3).each do |i|
+  parent = BlogCategory.create(
+    sort_order: i,
+    permalink: "permalink#{i}",
+    name: "category#{i}",
+    level: i
+  )
+  1.upto(3).each do |j|
+    child = BlogCategory.create(
+      sort_order: j,
+      permalink: "permalink#{i}-#{j}",
+      name: "category#{i}-#{j}",
+      level: j,
+      parent: parent
+    )
+    1.upto(3).each do |k|
+      BlogCategory.create(
+        sort_order: k,
+        permalink: "permalink#{i}-#{j}-#{k}",
+        name: "category#{i}-#{j}-#{k}",
+        level: k,
+        parent: child
+      )
+    end
+  end
+end
