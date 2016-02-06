edde7a75a226d1c044c427c1f5f7cd24f3c70bab
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-06 17:42:37 +0900

    spec/views/layouts/application_spec.rb のテスト文言修正

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'layouts/application.html.slim' do
-  it 'head 要素部分テンプレートがレンダリングされること' do
+  it '各種部分テンプレートがレンダリングされること' do
     view.lookup_context.prefixes = 'application'
     render
     expect(rendered).to render_template('_head')
