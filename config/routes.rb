e0597940ffd8a9048277ff53c5e3269cdb6089a2
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-06 17:16:57 +0900

    管理者メニューへのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,7 @@ Rails.application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  namespace :admin do
+    get 'menu'
+  end
 end
