243668b2779be2613a4d86eb0e7d9ad4ce840e27
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-14 17:37:29 +0900

    rails_helper.rb で FactoryGirl::Syntax::Methods をインクルード

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include FactoryGirl::Syntax::Methods
   config.include SessionHelpers
 
   # database_cleaner
