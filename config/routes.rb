1b6385841990da020afb724f6643b8d8a2b6c268
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-14 18:00:39 +0900

    annotate -r 実行 (resources 'blog_categories' の反映)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 # == Route Map
 #
-#         Prefix Verb   URI Pattern               Controller#Action
-#           root GET    /                         welcome#index
-#  admin_sign_in GET    /admin/sign_in(.:format)  admin/session#new
-#                POST   /admin/sign_in(.:format)  admin/session#create
-# admin_sign_out DELETE /admin/sign_out(.:format) admin/session#destroy
-#     admin_menu GET    /admin/menu(.:format)     admin/menu#index
-#          admin GET    /admin(.:format)          admin/menu#index
+#                   Prefix Verb   URI Pattern                               Controller#Action
+#                     root GET    /                                         welcome#index
+#            admin_sign_in GET    /admin/sign_in(.:format)                  admin/session#new
+#                          POST   /admin/sign_in(.:format)                  admin/session#create
+#           admin_sign_out DELETE /admin/sign_out(.:format)                 admin/session#destroy
+#               admin_menu GET    /admin/menu(.:format)                     admin/menu#index
+#    admin_blog_categories GET    /admin/blog_categories(.:format)          admin/blog_categories#index
+#                          POST   /admin/blog_categories(.:format)          admin/blog_categories#create
+#  new_admin_blog_category GET    /admin/blog_categories/new(.:format)      admin/blog_categories#new
+# edit_admin_blog_category GET    /admin/blog_categories/:id/edit(.:format) admin/blog_categories#edit
+#      admin_blog_category GET    /admin/blog_categories/:id(.:format)      admin/blog_categories#show
+#                          PATCH  /admin/blog_categories/:id(.:format)      admin/blog_categories#update
+#                          PUT    /admin/blog_categories/:id(.:format)      admin/blog_categories#update
+#                          DELETE /admin/blog_categories/:id(.:format)      admin/blog_categories#destroy
+#                    admin GET    /admin(.:format)                          admin/menu#index
 #
 
 Rails.application.routes.draw do
