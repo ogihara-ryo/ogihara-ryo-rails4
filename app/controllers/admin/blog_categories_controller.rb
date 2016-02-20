54024f076beecc469a31334700591595f715fb40
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-20 18:19:42 +0900

    BlogCategoriesController#show 実装

--- app/controllers/admin/blog_categories_controller.rb
+++ app/controllers/admin/blog_categories_controller.rb
@@ -1,4 +1,15 @@
 class Admin::BlogCategoriesController < Admin::SessionController
+  before_action :set_blog_category, only: :show
+
   def index
+  end
+
+  def show
+  end
+
+  private
+
+  def set_blog_category
+    @blog_category = BlogCategory.find(params[:id])
   end
 end
--- spec/controllers/admin/blog_categories_controller_spec.rb
+++ spec/controllers/admin/blog_categories_controller_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Admin::BlogCategoriesController, type: :controller do
     before { get :index }
     it { is_expected.to have_http_status(:success) }
   end
+
+  describe 'GET #show' do
+    before do
+      @blog_category = create(:blog_category)
+      get :show, id: @blog_category.id
+    end
+    it { is_expected.to have_http_status(:success) }
+    it { expect(assigns(:blog_category)).to eq @blog_category }
+  end
 end
