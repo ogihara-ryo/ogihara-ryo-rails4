c9299ad5400fbe14fc7c49323600ed94e883b3d2
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-24 21:36:59 +0900

    サインイン時に元ページへリダイレクトする処理のテストを改修

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -8,11 +8,10 @@ feature 'サインイン後のリダイレクト先' do
     expect(current_path).to eq admin_menu_path
   end
 
-  # TODO: 別のページを作ったらパスを差し替える
   scenario '別のページからサインインページにリダイレクトされた場合は元のページへ' do
-    visit admin_menu_path
+    visit admin_blog_categories_path
     fill_in 'session_password', with: ENV['ADMIN_PASSWORD']
     click_button I18n.t('admin.sign_in')
-    expect(current_path).to eq admin_menu_path
+    expect(current_path).to eq admin_blog_categories_path
   end
 end
