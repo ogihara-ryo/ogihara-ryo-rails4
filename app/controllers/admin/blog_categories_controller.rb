289f3e7a93048d5ab7e145c559664449391e48d4
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-02-27 18:41:23 +0900

    BlogCategoriesController に create アクションを実装

--- app/controllers/admin/blog_categories_controller.rb
+++ app/controllers/admin/blog_categories_controller.rb
@@ -12,9 +12,27 @@ class Admin::BlogCategoriesController < Admin::SessionController
     @blog_category = BlogCategory.new
   end
 
+  def create
+    @blog_category = BlogCategory.new(blog_category_params)
+    if @blog_category.save
+      redirect_to admin_blog_category_path(@blog_category)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_blog_category
     @blog_category = BlogCategory.find(params[:id])
+  end
+
+  def blog_category_params
+    params.require(:blog_category).permit(
+      :sort_order,
+      :permalink,
+      :name,
+      :parent_id
+    )
   end
 end
--- spec/controllers/admin/blog_categories_controller_spec.rb
+++ spec/controllers/admin/blog_categories_controller_spec.rb
@@ -23,4 +23,15 @@ RSpec.describe Admin::BlogCategoriesController, type: :controller do
     it { is_expected.to have_http_status(:success) }
     it { expect(assigns(:blog_category).new_record?).to be_truthy }
   end
+
+  describe 'POST #create' do
+    context '成功の場合' do
+      it 'BlogCategory インスタンスが1つ作成され、Show ページへリダイレクトされること' do
+        expect {
+          post :create, blog_category: attributes_for(:blog_category)
+        }.to change(BlogCategory, :count)
+        expect(response).to redirect_to admin_blog_category_path(assigns[:blog_category])
+      end
+    end
+  end
 end
