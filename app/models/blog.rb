3043cc3acebe3670c198383e0bf94929d727d829
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-03-16 14:26:50 +0900

    annotate 実行

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  content     :text
+#  category_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Blog < ActiveRecord::Base
   belongs_to :category
 end
