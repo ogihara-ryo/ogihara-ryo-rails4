c95010dbf6642821c867b1a5f5a051436abf6894
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-03-26 00:28:40 +0900

    BlogsController#index 実装

---
+++ app/controllers/admin/blogs_controller.rb
@@ -0,0 +1,4 @@
+class Admin::BlogsController < ApplicationController
+  def index
+  end
+end
---
+++ spec/controllers/admin/blogs_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Admin::BlogsController, type: :controller do
+  before { sign_in }
+  subject { response }
+
+  describe 'GET #index' do
+    before { get :index }
+    it { is_expected.to have_http_status(:success) }
+  end
+end
