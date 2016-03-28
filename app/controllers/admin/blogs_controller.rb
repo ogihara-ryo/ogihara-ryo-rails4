d2bfa497c76ffef41f29599a77c7d6709d248e8c
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-03-28 09:51:32 +0900

    BlogsController#show 実装

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -1,5 +1,16 @@
 class Admin::BlogsController < ApplicationController
+  before_action :set_blog, only: %i(show)
+
   def index
     @blogs = Blog.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
--- spec/controllers/admin/blogs_controller_spec.rb
+++ spec/controllers/admin/blogs_controller_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Admin::BlogsController, type: :controller do
     before { get :index }
     it { is_expected.to have_http_status(:success) }
   end
+
+  describe 'GET #show' do
+    before do
+      @blog = create(:blog)
+      get :show, id: @blog.id
+    end
+    it { is_expected.to have_http_status(:success) }
+    it { expect(assigns(:blog)).to eq @blog }
+  end
 end
