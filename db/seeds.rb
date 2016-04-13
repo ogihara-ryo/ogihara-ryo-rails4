1279747997a62c2c37e1557930d64da977804600
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-04-13 18:45:26 +0900

    seed ファイルに Blog インスタンスの生成処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 BlogCategory.delete_all
 BlogCategory.connection.execute(%|SELECT setval ('blog_categories_id_seq', 1, false)|)
+Blog.delete_all
+Blog.connection.execute(%|SELECT setval ('blogs_id_seq', 1, false)|)
 
 1.upto(3).each do |i|
   parent = BlogCategory.create(
@@ -22,5 +24,16 @@ BlogCategory.connection.execute(%|SELECT setval ('blog_categories_id_seq', 1, fa
         parent: child
       )
     end
+  end
+end
+
+BlogCategory.all.each do |blog_category|
+  1.upto(3).each do |i|
+    Blog.create(
+      title: "#{blog_category.name}-title#{i}",
+      content: "#{blog_category.name}-content#{i}",
+      category: blog_category,
+      draft: i.even?
+    )
   end
 end
