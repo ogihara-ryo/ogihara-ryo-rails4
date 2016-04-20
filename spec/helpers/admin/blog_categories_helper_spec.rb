a9b895259324755554b9bf2d01911bea88e44c53
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-04-20 12:57:50 +0900

    blog_categories_helper_spec リファクタリング

--- spec/helpers/admin/blog_categories_helper_spec.rb
+++ spec/helpers/admin/blog_categories_helper_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Admin::BlogCategoriesHelper do
   describe '#render_blog_categories_hierarchy' do
-    before do
-      @blog_categories = create_list(:blog_category, 3)
-      @blog_categories.first.parent = nil
-      @blog_categories.second.parent = @blog_categories.first
-      @blog_categories.third.parent = @blog_categories.second
-    end
+    subject { render_blog_categories_hierarchy(blog_categories) }
 
-    subject { render_blog_categories_hierarchy(@blog_categories) }
+    let(:blog_categories) { create_list(:blog_category, 3) }
+
+    before do
+      blog_categories.first.parent = nil
+      blog_categories.second.parent = blog_categories.first
+      blog_categories.third.parent = blog_categories.second
+    end
 
     it { is_expected.not_to eq be_nil }
   end
@@ -18,21 +19,19 @@ RSpec.describe Admin::BlogCategoriesHelper do
     subject { link_to_parent_blog_category }
 
     context '親カテゴリーがある場合' do
-      before do
-        @parent_blog_category = create(:blog_category, parent: nil)
-        @child_blog_category = create(:blog_category, parent: @parent_blog_category)
-      end
+      let(:parent_blog_category) { create(:blog_category, parent: nil) }
+      let(:child_blog_category) { create(:blog_category, parent: parent_blog_category) }
+
       it '親カテゴリーの Show ページへのリンクを返すこと' do
-        expect(link_to_parent_blog_category(@child_blog_category))
-          .to eq link_to(@parent_blog_category.name, admin_blog_category_path(@parent_blog_category))
+        expect(link_to_parent_blog_category(child_blog_category))
+          .to eq link_to(parent_blog_category.name, admin_blog_category_path(parent_blog_category))
       end
     end
 
     context '親カテゴリーがない場合' do
-      before do
-        @blog_category = create(:blog_category, parent: nil)
-      end
-      it { expect(link_to_parent_blog_category(@blog_category)).to eq I18n.t('text_none_parent') }
+      let(:blog_category) { create(:blog_category, parent: nil) }
+
+      it { expect(link_to_parent_blog_category(blog_category)).to eq I18n.t('text_none_parent') }
     end
   end
 end
